9b690e755a50f28ab6ffb8152eabf804ad66e4d2
Goodness Okanlawon <102490495+CodeEvolver@users.noreply.github.com>
2024-07-06 17:59:07 +0100

    mobile ehancement.

--- src/Components/CTASection.jsx
+++ src/Components/CTASection.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ArrowRight from '../assets/icons/filled-arrow-right.png'
 const CTASection = () => {
   return (
-    <div className='flex flex-col p-[2rem] w-[100%] max-w-[100vw] gap-[var(--custom-gap)] bg-[var(--faint-blue-background)] sm:flex-row sm:justify-around sm:gap-0 sm:px-[4rem]'>
-      <div className='flex flex-col items-center gap-[var(--custom-gap)] text-center border-2 p-[var(--custom-padding)] rounded-[0.5rem] sm:w-[40%]'>
+    <div className='flex flex-col py-[3rem]  px-[1.5rem] w-[100%] max-w-[100vw] gap-[var(--custom-gap)] bg-[var(--faint-blue-background)] sm:flex-row sm:justify-around sm:gap-0 sm:px-[4rem] sm:py-[3rem]'>
+      <div className='flex flex-col items-center gap-[var(--custom-gap)] text-center border-2 p-[1.5rem] rounded-[0.5rem] sm:w-[40%] sm:p-[var(--custom-padding)]'>
         <h2 className='text-[var(--secondary-color)] text-[2.5rem] font-semibold leading-[3rem]'>Transcribe ASL <br/>to Text</h2>
         <button tabIndex={0} className='flex flex-row items-center gap-[0.25rem] border-[0.16rem] border-solid border-[var(--secondary-color)] bg-[var(--blue-background)] p-[var(--button-padding)] rounded-[0.25rem] text-[var(--tertiary-color)] text-[1rem] font-normal sm:w-[max-content]'>Contribute <img src={ArrowRight} className='h-[0.8rem]'/></button>
       </div>
-      <div className='flex flex-col items-center gap-[var(--custom-gap)] text-center border-2 p-[var(--custom-padding)] rounded-[0.5rem] sm:w-[40%]'>
+      <div className='flex flex-col items-center gap-[var(--custom-gap)] text-center border-2 p-[1.5rem] rounded-[0.5rem] sm:w-[40%] sm:p-[var(--custom-padding)]'>
         <h2 className='text-[var(--secondary-color)] text-[2.5rem] font-semibold leading-[3rem]'>Transcribe Text <br/>to ASL</h2>
         <button tabIndex={0} className='flex flex-row items-center gap-[0.25rem] border-[0.16rem] border-solid border-[var(--secondary-color)] bg-[var(--blue-background)] p-[var(--button-padding)] rounded-[0.25rem] text-[var(--tertiary-color)] text-[1rem] font-normal sm:w-[max-content]'>Contribute <img src={ArrowRight} className='h-[0.8rem]'/></button>
       </div>
--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -3,7 +3,7 @@ import EnvelopeIcon from '../assets/icons/closed-blue-envelope.png'
 
 const ContactUs = () => {
   return (
-    <div className='flex flex-col gap-[var(--custom-gap)] w-[100%] max-w-[100vw] p-[var(--custom-padding)] bg-[#F3F4FF] sm:flex-row sm:flex-wrap sm:gap-0 '>
+    <div className='flex flex-col gap-[var(--custom-gap)] w-[100%] max-w-[100vw] py-[3rem] px-[1.5rem] sm:p-[var(--custom-padding)] bg-[#F3F4FF] sm:flex-row sm:flex-wrap sm:gap-0 '>
       <div className='w-[100%] sm:w-[50%]'>
         <h1 className='text-[var(--primary-color)] text-[4rem] leading-[4.8rem] font-bold'>Contact <br/><span className='text-[var(--secondary-color)]'>Us</span></h1>
         <p className='text-[var(--sub-headline-color)] text-[1.25rem] leading-[1.5rem]'>Get in touch with us. Feel free to help us with suggestions on how to improve this product.</p>
