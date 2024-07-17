48698399ec3c9bbf62521e33d112449f9dc1418c
Goodness Okanlawon <102490495+CodeEvolver@users.noreply.github.com>
2024-07-17 14:03:12 +0100

    UI Engineering, Record Video, Translate Text.

--- src/Pages/RecordVideo.jsx
+++ src/Pages/RecordVideo.jsx
@@ -8,35 +8,35 @@ import ArrowIcon from '../assets/icons/filled-arrow-right.png'
 
 const RecordVideo = () => {
   return (
-    <div className='bg-black'>
-      <div>
-        <h1>Record Translation</h1>
-        <p>Capture your sign language translation for a given text</p>
+    <div className='flex flex-col justify-center gap-[var(--custom-gap)] bg-[var(--tertiary-background)] w-[100%] sm:h-[calc(100vh-97.19px)] max-w-[100vw] py-[3rem] px-[1.5rem] sm:p-[var(--custom-padding)]'>
+      <div className='flex flex-col'>
+        <h1 className='font-bold text-[2rem]'>Record Translation</h1>
+        <p className='text-[1rem] text-[var(--input-color)]'>Capture your sign language translation for a given text</p>
       </div>
-      <div>
-        <div>
-          <div>
-            <div>
-              <img src={PlayIcon}/>
-              <img src={StopIcon}/>
-              <img src=""/>
+      <div className='flex flex-row gap-[2.5rem] w-[100%]'>
+        <div className='flex flex-col gap-[var(--custom-gap)] w-[100%] sm:w-[50%]'>
+          <div className='flex flex-row items-end justify-center h-[50vh] w-[100%] sm:w-[100%] bg-[var(--black-background)]'>
+            <div className='flex flex-row gap-[var(--custom-gap)] mb-[calc(2*var(--custom-gap))]'>
+              <img className='cursor-pointer' src={PlayIcon}/>
+              <img className='cursor-pointer' src={StopIcon}/>
+              <img className='cursor-pointer' src={PlayIcon}/>
             </div>
           </div>
-          <div>
-            <select placeholder="Select Sign Language">
-              <option>ASL</option>
+          <div className='flex flex-row justify-between'>
+            <select tabIndex={0} className='p-[var(--button-padding)] rounded-[0.5rem] text-[var(--input-color)] sm:w-[50%]'>
+              <option value="" className='p-[var(--button-padding)] rounded-[0.5rem] text-[var(--input-color)] sm:w-[50%]'>Select Sign Language</option>
             </select>
-            <button>Upload <img src={UploadIcon}></img></button>
+            <button tabIndex={0} className='bg-[var(--blue-background)] p-[var(--button-padding)] rounded-[0.5rem] text-[var(--tertiary-color)] font-semibold sm:p-[var(--button-padding)] shadow-[var(--button-shadow)] gap-[var(--inline-gap)]'>Upload <img src={UploadIcon}></img></button>
           </div>
         </div>
-        <div>
-          <div>
-            <input type='slide'/>
-            <p>Increased popularity has given rise to problems of congestion and erosion</p>
+        <div className='flex flex-col gap-[var(--custom-gap)] w-[100%] sm:w-[50%]'>
+          <div className='flex flex-col items-center justify-center p-[var(--button-padding)] bg-[var(--white-background)] h-[50vh] w-[100%] sm:w-[100%]'>
+            <p className='text-center text-[20px] font-semibold text-wrap'>Increased popularity has given rise to problems of congestion and erosion</p>
           </div>
-          <div>
-            <button><img className='rotate-180' src={ArrowIcon}/>Previous</button>
-            <button>Next <img src={ArrowIcon} /></button>
+          <div className='flex flex-row justify-between'>
+            <button className='bg-[var(--blue-background)] opacity-[0.3] p-[var(--button-padding)] rounded-[0.5rem] text-[var(--tertiary-color)] font-semibold shadow-[var(--button-shadow)] gap-[var(--inline-gap)] sm:p-[var(--button-padding)]'><img className='rotate-180' src={ArrowIcon}/>Previous</button>
+            <button className='underline'>Skip</button>
+            <button className='bg-[var(--blue-background)] p-[var(--button-padding)] rounded-[0.5rem] text-[var(--tertiary-color)] font-semibold shadow-[var(--button-shadow)] gap-[var(--inline-gap)] sm:p-[var(--button-padding)]'>Next <img src={ArrowIcon} /></button>
           </div>
         </div>
       </div>
--- src/Pages/TranslateText.jsx
+++ src/Pages/TranslateText.jsx
@@ -6,30 +6,36 @@ import ArrowDown from '../assets/icons/filled-arrow-down.png'
 
 const TranslateText = () => {
   return (
-    <div className='bg-black'>
-      <div>
-        <h1>Translate Video</h1>
-        <p>Type in the text translation for the given video</p>
+    <div className='flex flex-col justify-center gap-[var(--custom-gap)] bg-[var(--tertiary-background)] w-[100%] sm:h-[calc(100vh-97.19px)] max-w-[100vw] py-[3rem] px-[1.5rem] sm:p-[var(--custom-padding)]'>
+      <div className='flex flex-col'>
+        <h1 className='font-bold text-[2rem]'>Translate Video</h1>
+        <p className='text-[1rem] text-[var(--input-color)]'>Type in the text translation for the given video</p>
       </div>
-      <div>
-        <div>
-          <div>
-            <p>ASL</p>
-            <img src={ArrowDown}/>
+      <div className='flex flex-row gap-[2.5rem] w-[100%]'>
+        <div className='flex flex-col gap-[var(--custom-gap)] w-[100%] sm:w-[50%]'>
+          <div className='flex flex-row items-end justify-center h-[50vh] w-[100%] sm:w-[100%] bg-[var(--black-background)]'>
+            {/* <div className='flex flex-row items-center absolute bg-[var(--white-background)] p-[var(--button-padding)] mt-0'>
+              <p>ASL</p>
+              <img src={ArrowDown}/>
+            </div> */}
+            <div className='flex flex-row gap-[var(--custom-gap)] mb-[calc(2*var(--custom-gap))]'>
+              <img src={PlayIcon} className='cursor-pointer'/>
+              <img src={StopIcon} className='cursor-pointer'/>
+            </div>
           </div>
-          <div>
-            <img src={PlayIcon}/>
-            <img src={StopIcon}/>
+          <div className='flex flex-row justify-between'>
+            <button className='bg-[var(--blue-background)] opacity-[0.3] p-[var(--button-padding)] rounded-[0.5rem] text-[var(--tertiary-color)] font-semibold shadow-[var(--button-shadow)] gap-[var(--inline-gap)] sm:p-[var(--button-padding)]'><img className='rotate-180' src={ArrowIcon}/>Previous</button>
+            <button className='bg-[var(--blue-background)] p-[var(--button-padding)] rounded-[0.5rem] text-[var(--tertiary-color)] font-semibold shadow-[var(--button-shadow)] gap-[var(--inline-gap)] sm:p-[var(--button-padding)]'>Next <img src={ArrowIcon} /></button>
           </div>
         </div>
-        <div>
-          <input type='slide' />
-          <p>Enter text here</p>
+        <div className='flex flex-col gap-[var(--custom-gap)] w-[100%] sm:w-[50%]'>
+          <div className='flex flex-col items-center justify-center p-[var(--button-padding)] bg-[var(--white-background)] h-[50vh] w-[100%] sm:w-[100%]'>
+            <textarea className='text-center align-middle text-[2rem] text-[var(--input-color)] font-semibold text-wrap w-[100%] h-auto'>Enter text here...</textarea>
+          </div>
+          <div className='flex flex-row justify-end'>
+            <button className='bg-[var(--blue-background)] p-[var(--button-padding)] rounded-[0.5rem] text-[var(--tertiary-color)] font-semibold shadow-[var(--button-shadow)] gap-[var(--inline-gap)] sm:p-[var(--button-padding)]'>Submit <img src={ArrowIcon} /></button>
+          </div>
         </div>
-      </div>
-      <div>
-        <button><img className='rotate-180' src={ArrowIcon}/> Previous</button>
-        <button>Submit <img src={ArrowIcon}/></button>
       </div>
     </div>
   )
