20f6d5f82ce8f932cd123440258f0ac86b9162bb
Goodness Okanlawon <102490495+CodeEvolver@users.noreply.github.com>
2024-07-29 14:25:48 +0100

    content-type mod

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -62,7 +62,7 @@ const ContactUs = () => {
       method:"POST",
       body: JSON.stringify(data),
       headers: {
-        "Content-Type": "application/json; charset=utf-8"
+        "Content-Type": "application/json"
       }
     })
     .then(res => res.json())
